fd9e3a439687b3e568d2c16e51b3ea46a885dc09
“Samariddin <samariddinnurmamatov111@gmail.com>
2023-08-03 01:20:16 +0500

    RTK-QUERY-CRUD

--- src/pages/HomeP.jsx
+++ src/pages/HomeP.jsx
@@ -1,7 +1,7 @@
 
 const HomeP = () => {
   return (
-    <div>Bu sahifamiz Crud ga asoslangan mocapi orqali qilingan categoriesP qilingan</div>
+    <div className="containe">HomeP Bu website mocApi crud qilingan: CategoriesP da <br /><br /><br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eaque ullam illum. Non, pariatur quos! Asperiores earum minima placeat, quisquam dolorum deserunt sapiente unde voluptatum officia excepturi voluptas minus consequatur accusamus. Possimus, ad odit ullam similique, ab minima eaque dolore blanditiis sit harum quae ea illo quia. Dolorum explicabo veritatis sunt quisquam reprehenderit voluptas ad non aspernatur, excepturi blanditiis porro repellendus, voluptates officia maxime, suscipit aperiam laudantium. Officiis animi vitae exercitationem magnam dolorum fugit pariatur laudantium dolor doloribus? Ut nam, quasi minus totam magni, autem praesentium perspiciatis possimus aut dicta architecto impedit voluptas quam quos fuga, eligendi nemo suscipit inventore.</div>
   )
 }
 
